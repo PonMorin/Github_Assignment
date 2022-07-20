6b500e530f6843258317b3d44318abc57427c66f
Pon <pon.m22@gmail.com>
2022-07-20 21:07:27 +0700

    ChangeThisFile2

--- Digital_clock/digital_clock.py
+++ Digital_clock/digital_clock.py
@@ -2,32 +2,32 @@ import tkinter as tk
 from time import strftime
 
 
-# def light_theme():
-#     frame = tk.Frame(root, bg="white")
-#     frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
-#     lbl_1 = tk.Label(frame, font=('calibri', 40, 'bold'),
-#                      bg='#fff', fg='#f00')
-#     lbl_1.pack(anchor="s")
+def light_theme():
+    frame = tk.Frame(root, bg="#ACDDDE")
+    frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+    lbl_1 = tk.Label(frame, font=('calibri', 40, 'bold'),
+                     bg='#fff', fg='#f00')
+    lbl_1.pack(anchor="s")
 
-#     def time():
-#         string = strftime('%I:%M:%S %p')
-#         lbl_1.config(text=string)
-#         lbl_1.after(1000, time)
-#     time()
+    def time():
+        string = strftime('%I:%M:%S %p')
+        lbl_1.config(text=string)
+        lbl_1.after(1000, time)
+    time()
 
 
-# def dark_theme():
-#     frame = tk.Frame(root, bg="#66FFFF	")
-#     frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
-#     lbl_2 = tk.Label(frame, font=('calibri', 40, 'bold'),
-#                      bg='#fff', fg='#f00')
-#     lbl_2.pack(anchor="s")
+def dark_theme():
+    frame = tk.Frame(root, bg="#018786")
+    frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+    lbl_2 = tk.Label(frame, font=('calibri', 40, 'bold'),
+                     bg='#fff', fg='#f00')
+    lbl_2.pack(anchor="s")
 
-#     def time():
-#         string = strftime('%I:%M:%S %p')
-#         lbl_2.config(text=string)
-#         lbl_2.after(1000, time)
-#     time()
+    def time():
+        string = strftime('%I:%M:%S %p')
+        lbl_2.config(text=string)
+        lbl_2.after(1000, time)
+    time()
 
 
 root = tk.Tk()
@@ -49,8 +49,8 @@ time()
 
 menubar = tk.Menu(root)
 theme_menu = tk.Menu(menubar, tearoff=0)
-# theme_menu.add_command(label="Light", command=light_theme)
-# theme_menu.add_command(label="Dark", command=dark_theme)
+theme_menu.add_command(label="Light", command=light_theme)
+theme_menu.add_command(label="Dark", command=dark_theme)
 menubar.add_cascade(label="Theme", menu=theme_menu)
 root.config(menu=menubar)
 root.mainloop()
